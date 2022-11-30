d86f477ea48565efd0c0881cdaccfea814edb69a
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-30 15:15:59 +0700

    41

--- Program.cs
+++ Program.cs
@@ -1,17 +1,25 @@
-﻿ int [] GetUserArray (int length, int start, int end)
-            {
-                Console.Write($"Введите количество чисел: ");
-                length = int.Parse(Console.ReadLine());
-                int [] array = new int [length];
-                for (int i = 0; i < array.Length; i++)
-                {
-                    Console.Write($"Введите число: ");
-                    array[i] = int.Parse(Console.ReadLine());                    
-                }
-                return array;
-                
-            }
-            void PrintArray(int [] array)
+﻿Console.Write("Введите число М: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите число {i+1}: ");
+        array[i] = Int32.Parse(Console.ReadLine());
+    }
+    int pozitiv(int[] array)
+    {
+    int count = 0;
+      for (int i = 0; i < array.Length; i++)
+      {
+          if (array[i] > 0)
+          {
+              count++;
+          }
+      } 
+    return count;
+    }
+ 
+ void PrintArray(int [] array)
             {
                 Console.Write("[");
                 for (int i = 0; i < array.Length; i++)
@@ -24,23 +32,6 @@
                 }
                 Console.WriteLine("]");
             }
-
-            int Positiv (int [] array)
-            {
-                int count = 0;
-                for (int i = 0; i < array.Length; i++)
-                {
-                    if (array[i] > 0)
-                    {
-                        count++;
-                    }
-                }
-                return count;
-            }
-
-            int [] newArray = GetUserArray(5,-10, 10);
-            Console.WriteLine();
-            PrintArray(newArray);
-            int countNumber = Positiv(newArray);
-            Console.WriteLine();
-            Console.WriteLine ($"Количество положительных чисел -  {countNumber}");
+ PrintArray(array);           
+ int Pozitiv = pozitiv(array);            
+Console.WriteLine($"Количество чисел больше 0 - {Pozitiv}");
