25fa3b61b9b419419ce4dbe1d3bc8c2c877614f7
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-30 12:34:08 +0700

    41

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿ int [] GetUserArray (int length, int start, int end)
+            {
+                Console.Write($"Введите количество чисел: ");
+                length = int.Parse(Console.ReadLine());
+                int [] array = new int [length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"Введите число: ");
+                    array[i] = int.Parse(Console.ReadLine());                    
+                }
+                return array;
+                
+            }
+            void PrintArray(int [] array)
+            {
+                Console.Write("[");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine("]");
+            }
+
+            int Positiv (int [] array)
+            {
+                int count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            int [] newArray = GetUserArray(5,-10, 10);
+            Console.WriteLine();
+            PrintArray(newArray);
+            int countNumber = Positiv(newArray);
+            Console.WriteLine();
+            Console.WriteLine ($"Количество положительных чисел -  {countNumber}");
